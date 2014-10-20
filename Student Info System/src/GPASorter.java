c7cb20039a55bcf0e4f451424157537497aaac40
zoe.ryan <zoe.ryan@MU-C3-23.mullen.pvt.k12.co.us>
2014-10-20 13:32:38 -0600

    GPA Sorter

--- Student Info System/src/GPASorter.java
+++ Student Info System/src/GPASorter.java
@@ -1,5 +1,13 @@
+import java.util.Comparator;
 
 public class GPASorter
 	{
-
+	public int compare (Student s1 , Student s2)
+		{
+		if(s1.getScore() < s2.getScore())
+			return 1;
+		else
+			return -1;
+		
+		}
 	}
