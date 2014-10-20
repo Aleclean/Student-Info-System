aa1050291bb319b4648f6401585bce640cf0f534
zoe.ryan <zoe.ryan@MU-C3-23.mullen.pvt.k12.co.us>
2014-10-20 13:40:49 -0600

    Student ArrayList

--- Student Info System/src/StudentList.java
+++ Student Info System/src/StudentList.java
@@ -1,5 +1,31 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class StudentList
 	{
+	Scanner file = new Scanner (new File ("studentlist.txt"));
 
+//	Scanner file = new Scanner (new File ("snacks.txt"));
+//	int numberOfLinesOfData = file.nextInt();
+	ArrayList <Student> studentList = new ArrayList();
+	
+//			
+//	ArrayList <Product> products = new ArrayList();
+//	for(int i = 0; i < numberOfLinesOfData; i ++)
+//	import SIS file
+//	import java.util.ArrayList;
+//	constructor
+//		first name String
+//		last name String
+//		Course one Stirng
+//		grade one String
+//		Course two String
+//		grade two String
+//		Course three String
+//		grade three Stirng
+//		GPA double
+//			getters/setters
+//	makeStudentList();
+//	add Students to an arrayList
+//			ArrayList <Student> StudentList = new ArrayList <Student>(); 
 	}
