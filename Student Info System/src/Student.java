e0784b09dcc175c9e9dd4b927f56df5d21a17da6
grace.hickey <grace.hickey@MU-C3-22.mullen.pvt.k12.co.us>
2014-10-21 12:43:26 -0600

    Commit

--- Student Info System/src/Student.java
+++ Student Info System/src/Student.java
@@ -1,5 +1,9 @@
 
 public class Student
 	{
-	//commit test
+	System.out.println("What is your name?");
+	studentList.add(firstName, lastName, course, );
+	studentList.remove();
 	}
+//add and delete students from the ArrayList
+
