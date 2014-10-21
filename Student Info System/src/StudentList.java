83e92bf6912894679dbe63d0c77901044aaf7af1
zoe.ryan <zoe.ryan@MU-C3-23.mullen.pvt.k12.co.us>
2014-10-21 12:15:12 -0600

    Constructor

--- Student Info System/src/StudentList.java
+++ Student Info System/src/StudentList.java
@@ -1,9 +1,45 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+
+
 public class StudentList
 	{
+	
+	private String lastName;
+	private String firstName;
+	private String courseOne;
+	private String gradeOne;
+	private String courseTwo;
+	private String gradeTwo;
+	private String courseThree;
+	private String gradeThree;
+	private double gpa;
+	
 	Scanner file = new Scanner (new File ("studentlist.txt"));
+	
+
+	public StudentList(String l, String f, String co, String go, String ct, String gt, String cth, String gth, double g)
+	{
+		lastName = l;
+		firstName = f;
+		courseOne = co;
+		gradeOne = go;
+		courseTwo = ct;
+		gradeTwo = gt;
+		courseThree = cth;
+		gradeThree = gth;
+		gpa = g;
+	}
+	
+	
+		
+	// loop through data file and add to array list
+	// constructor needs all 10 parts
+	
+	
+	ArrayList <Student> StudentList = new ArrayList <Student>();
+	}
 
 //	Scanner file = new Scanner (new File ("snacks.txt"));
 //	int numberOfLinesOfData = file.nextInt();
