229667d31d23cf5f9bb2d37c319d721bdf3754bd
alec.lean <alec.lean@MU-C3-24.mullen.pvt.k12.co.us>
2014-10-21 12:57:15 -0600

    Removed 2 classes

--- Student Info System/src/StudentRunner.java
+++ Student Info System/src/StudentRunner.java
@@ -6,7 +6,7 @@ public class StudentRunner
 		{
 		promptUser();
 		}
-	
+
 	public static void promptUser()
 		{
 		System.out.println("Welcome! What would you like to do?");
@@ -15,27 +15,71 @@ public class StudentRunner
 		System.out.println("3). Sort students");
 		Scanner userInput1 = new Scanner(System.in);
 		int choice = userInput1.nextInt();
-		
-		if(choice == 1)
+
+		if (choice == 1)
 			{
 			System.out.println("1). Add a student");
 			System.out.println("2). Remove a student");
 			int choice2 = userInput1.nextInt();
+
+			switch (choice2)
+				{
+				case 1:
+					{
+					addStudent();
+					break;
+					}
+				case 2:
+					{
+					removeStudent();
+					break;
+					}
+				}
 			}
-		
-		if(choice == 2)
+
+		if (choice == 2)
 			{
 			System.out.println("1). Change grades");
 			System.out.println("2). Change classes");
 			int choice2 = userInput1.nextInt();
+
+			switch (choice2)
+				{
+				case 1:
+					{
+					changeGrade();
+					break;
+					}
+				case 2:
+					{
+					changeClass();
+					break;
+					}
+				}
 			}
-		
-		if(choice == 3)
+
+		if (choice == 3)
 			{
 			System.out.println("1). Sort by last name");
 			System.out.println("2). Sort by GPA");
 			System.out.println("3). Sort by class");
 			int choice2 = userInput1.nextInt();
+
+			switch (choice2)
+				{
+				case 1:
+					{
+					addStudent();
+					}
+				case 2:
+					{
+					removeStudent();
+					}
+				case 3:
+					{
+					
+					}
+				}
 			}
 		}
 	}
