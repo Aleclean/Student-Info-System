f876d57d02cac58510953434c51f21916179e82a
alec.lean <alec.lean@MU-C3-24.mullen.pvt.k12.co.us>
2014-10-21 12:50:38 -0600

    User input added

--- Student Info System/src/StudentRunner.java
+++ Student Info System/src/StudentRunner.java
@@ -1,11 +1,41 @@
+import java.util.Scanner;
 
 public class StudentRunner
 	{
-
 	public static void main(String[] args)
 		{
-		// TODO Auto-generated method stub
-
+		promptUser();
 		}
-
+	
+	public static void promptUser()
+		{
+		System.out.println("Welcome! What would you like to do?");
+		System.out.println("1). Add or remove a student");
+		System.out.println("2). Change grades or schedule");
+		System.out.println("3). Sort students");
+		Scanner userInput1 = new Scanner(System.in);
+		int choice = userInput1.nextInt();
+		
+		if(choice == 1)
+			{
+			System.out.println("1). Add a student");
+			System.out.println("2). Remove a student");
+			int choice2 = userInput1.nextInt();
+			}
+		
+		if(choice == 2)
+			{
+			System.out.println("1). Change grades");
+			System.out.println("2). Change classes");
+			int choice2 = userInput1.nextInt();
+			}
+		
+		if(choice == 3)
+			{
+			System.out.println("1). Sort by last name");
+			System.out.println("2). Sort by GPA");
+			System.out.println("3). Sort by class");
+			int choice2 = userInput1.nextInt();
+			}
+		}
 	}
