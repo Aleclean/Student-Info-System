a7a92807c6b47d4861c5498102efe78d11c6d6ef
zoe.ryan <zoe.ryan@MU-C3-23.mullen.pvt.k12.co.us>
2014-10-22 14:08:53 -0600

    making GPA

--- Student Info System/src/StudentGrade.java
+++ Student Info System/src/StudentGrade.java
@@ -1,5 +1,62 @@
 
 public class StudentGrade
 	{
+	public void setGpa()
+	{
 
+		{
+	if(studentList.get(i).getGradeOne.equals("A+"))
+		{
+		
+		}
+	if(Student.get(i).getGradeOne.equals("A"))
+		{
+		
+		}
+	if(Student.gradeOne.equals("A-"))
+		{
+		
+		}
+	if(Student.gradeOne.equals("B+"))
+		{
+		
+		}
+	if(Student.gradeOne.equals("B"))
+		{
+		
+		}
+	if(Student.gradeOne.equals("B-"))
+		{
+		
+		}
+	if(Student.gradeOne.equals("C+"))
+		{
+		
+		}
+	if(Student.gradeOne.equals("C"))
+		{
+		
+		}
+	if(Student.gradeOne.equals("C-"))
+		{
+		
+		}
+	if(Student.gradeOne.equals("D+"))
+		{
+		
+		}
+	if(Student.gradeOne.equals("D"))
+		{
+		
+		}
+	if(Student.gradeOne.equals("D-"))
+		{
+		
+		}
+	if(Student.gradeOne.equals("F"))
+		{
+		
+		}
+		}
+	}
 	}
