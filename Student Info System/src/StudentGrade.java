794141781a8ce8e9d349346505b1a6db3cc45f39
zoe.ryan <zoe.ryan@MU-C3-23.mullen.pvt.k12.co.us>
2014-10-23 13:18:15 -0600

    worked on GPA

--- Student Info System/src/StudentGrade.java
+++ Student Info System/src/StudentGrade.java
@@ -3,60 +3,30 @@ public class StudentGrade
 	{
 	public void setGpa()
 	{
+	String letterGrade;
+	double gpa;
+	double gradePoints;
+	double totalGradePoints;
 
 		{
-	if(studentList.get(i).getGradeOne.equals("A+"))
+		
+	public static void retrieveGrades()
+	{
+	for(int i = 0; i < Database.file.size(); i ++)
+		letterGrade = Database.studentList.get(i).getGradeOne;
+		letterGrade = Database.studentList.get(i).getGradeTwo;
+		letterGrade = Database.studentList.get(i).getGradeTwo;
+
+	}
+	public static void setPoints()
 		{
 		
 		}
-	if(Student.get(i).getGradeOne.equals("A"))
-		{
+	
 		
-		}
-	if(Student.gradeOne.equals("A-"))
-		{
+	
 		
-		}
-	if(Student.gradeOne.equals("B+"))
-		{
-		
-		}
-	if(Student.gradeOne.equals("B"))
-		{
-		
-		}
-	if(Student.gradeOne.equals("B-"))
-		{
-		
-		}
-	if(Student.gradeOne.equals("C+"))
-		{
-		
-		}
-	if(Student.gradeOne.equals("C"))
-		{
-		
-		}
-	if(Student.gradeOne.equals("C-"))
-		{
-		
-		}
-	if(Student.gradeOne.equals("D+"))
-		{
-		
-		}
-	if(Student.gradeOne.equals("D"))
-		{
-		
-		}
-	if(Student.gradeOne.equals("D-"))
-		{
-		
-		}
-	if(Student.gradeOne.equals("F"))
-		{
-		
-		}
+		Database.studentList.get().setGpa((numberGradeOne + numberGradeTwo + numberGradeThree)/3);
 		}
 	}
-	}
+}
