1e6ec9c898ef7830f4d222b713de6d5bd749bddf
zoe.ryan <zoe.ryan@MU-C3-23.mullen.pvt.k12.co.us>
2014-10-24 13:10:52 -0600

    Finished GPA calculator

--- Student Info System/src/StudentGrade.java
+++ Student Info System/src/StudentGrade.java
@@ -1,99 +1,98 @@
 
 public class StudentGrade
 	{
-	public void setGpa()
-	{
-	String letterGrade;
-	double gpa;
-	double gradePoints;
-	double totalGradePoints;
-	
-		
-		
-	public static void retrieveGrades()
-	{
-	for(int i = 0; i < Database.studentList.file.size(); i ++)
-		letterGrade = Database.studentList.get(i).getGradeOne;
-		letterGrade = Database.studentList.get(i).getGradeTwo;
-		letterGrade = Database.studentList.get(i).getGradeTwo;
 
-	}
+
+	public static String letterGrade;
+	public static double totalGradePoints;
+
+	
+	public static void retrieveGrades()
+		{
+		for(int i = 0; i < Database.studentList.size(); i ++)
+			{
+			letterGrade = Database.studentList.get(i).getGradeOne();
+				setPoints();
+			letterGrade = Database.studentList.get(i).getGradeTwo();
+				setPoints();
+			letterGrade = Database.studentList.get(i).getGradeTwo();
+				setPoints();
+			Database.studentList.get(i).setGpa(totalGradePoints/3); 
+			double totalGradePoints = 0.0;
+			}
+		}
+	
 	public static void setPoints()
 		{
 		switch(letterGrade)
 		{
 			case "A+":
 				{
-				gradePoints = "4.3";
+				totalGradePoints = totalGradePoints + 4.3;
 				break;
 				}
 			case "A":
 				{
-				gradePoints = "4.0";
+				totalGradePoints = totalGradePoints + 4.0;
 				break;
 				}
 			case "A-":
 				{
-				gradePoints = "3.7";
+				totalGradePoints = totalGradePoints +  3.7;
 				break;
 				}
 			case "B+":
 				{
-				gradePoints = "3.3";
+				totalGradePoints = totalGradePoints +  3.3;
 				break;
 				}
 			case "B":
 				{
-				gradePoints = "3.0";
+				totalGradePoints = totalGradePoints +  3.0;
 				break;
 				}
 			case "B-":
 				{
-				gradePoints = "2.7";
+				totalGradePoints = totalGradePoints +  2.7;
 				break;
 				}
 			case "C+":
 				{
-				gradePoints = "2.3";
+				totalGradePoints = totalGradePoints +  2.3;
 				break;
 				}
 			case "C":
 				{
-				gradePoints = "2.0";
+				totalGradePoints = totalGradePoints +  2.0;
 				break;
 				}
 			case "C-":
 				{
-				gradePoints = "1.7";
+				totalGradePoints = totalGradePoints +  1.7;
 				break;
 				}
 			case "D+":
 				{
-				gradePoints = "1.3";
+				totalGradePoints = totalGradePoints +  1.3;
 				break;
 				}
 			case "D":
 				{
-				gradePoints = "1.0";
+				totalGradePoints = totalGradePoints +  1.0;
 				break;
 				}
 			case "D-":
 				{
-				gradePoints = "0.7";
+				totalGradePoints = totalGradePoints +  0.7;
 				break;
 				}
 			case "F":
 				{
-				gradePoints = "0.0";
+				totalGradePoints = totalGradePoints +  0.0;
 				break;
 				}
+
 		}
 		}
-	
-		
-	public static void setGPA()
-		{
-		Database.studentList.get().setGpa((letterGrade + numberGradeTwo + numberGradeThree)/3);\
-		}
-	}
-	}
+
+}
